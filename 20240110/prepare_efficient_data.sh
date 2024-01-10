0d527da45de7cf0e08b5f4c479a7d7a872b8afbc
MingLiiii <minglii@umd.edu>
2024-01-10 00:11:34 -0500

    modi

---
+++ 20240110/prepare_efficient_data.sh
@@ -0,0 +1,3 @@
+python download_uploaded_data.py --data_path Minglii/ee5 --save_path data/alpaca_gpt2_none_5per_sharegpt.json
+python download_uploaded_data.py --data_path Minglii/ee10 --save_path data/alpaca_gpt2_none_10per_sharegpt.json
+python download_uploaded_data.py --data_path Minglii/ee15 --save_path data/alpaca_gpt2_none_15per_sharegpt.json
