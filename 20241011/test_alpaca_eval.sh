1732d9c7df3d789c8426b7418f33ca01f1f0e1c1
MingLiiii <minglii@umd.edu>
2024-01-11 04:25:49 -0500

    modi

--- 20241011/test_alpaca_eval.sh
+++ 20241011/test_alpaca_eval.sh
@@ -19,5 +19,36 @@ python inference/alpaca_eval_generation.py \
 
 python inference/alpaca_eval_generation.py \
     --do_eval False \
-    --model_name_or_path /root/scratch/trained_models_fs/alpaca_sharegpt_format/ \
-    --model_name_tag alpaca_sharegpt_format \
+    --model_name_or_path /root/scratch/trained_models_fs/alpaca_sharegpt_format_1e5_5epo/ \
+    --model_name_tag alpaca_sharegpt_format_1e5_5epo \
+
+
+cd /root/scratch/compare_eval
+
+array=(
+    /root/scratch/trained_models_fs/alpaca_sharegpt_format_1e5_5epo/
+)
+for i in "${array[@]}"
+do
+    echo $i
+        python generation/generation.py \
+            --dataset_name wizardlm \
+            --prompt vicuna \
+            --model_name_or_path $i \
+            --max_length 2048 
+
+done
+
+array=(
+    /root/scratch/trained_models_fs/alpaca_sharegpt_format_1e5_5epo/
+)
+for i in "${array[@]}"
+do
+    echo $i
+        python generation/generation.py \
+            --dataset_name vicuna \
+            --prompt vicuna \
+            --model_name_or_path $i \
+            --max_length 2048 
+            
+done
